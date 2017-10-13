e4b7e594bab62c24f517e8fdf80d7193cfea1c74
mayagold <maya.gold.1991@gmail.com>
2017-10-13 13:05:49 -0700

    user can now store location queries between browser sessions

--- public/app.js
+++ public/app.js
@@ -1,15 +1,11 @@
 var app = angular.module('skycastApp', []);
-
-
+var input = '';
 
 ////////////////////
 // GOOGLE MAPS
 // Resource: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
 // This code allows user to search for a specific city using Google Maps and retrieve its lat/lng coordinates. It also auto-updates a google maps view of that location.
 ////////////////////
-
-var input = '';
-
 function initAutocomplete() {
   // Render map on page: default to Seattle.
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -17,268 +13,268 @@ function initAutocomplete() {
     zoom: 11,
     mapTypeId: 'roadmap',
     styles: [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      // Exported from https://mapstyle.withgoogle.com/
       {
-        "color": "#212121"
+        "elementType": "geometry",
+        "stylers": [
+          // Exported from https://mapstyle.withgoogle.com/
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "stylers": [
+          {
+            "color": "#963f42"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#181818"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1b1b1b"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#2c2c2c"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#8a8a8a"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#373737"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#3c3c3c"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#4e4e4e"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#3d3d3d"
+          }
+        ]
       }
     ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#212121"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.country",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.locality",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.man_made",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.natural",
-    "stylers": [
-      {
-        "color": "#963f42"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.business",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#181818"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1b1b1b"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#2c2c2c"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#8a8a8a"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#373737"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#3c3c3c"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#4e4e4e"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#000000"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#3d3d3d"
-      }
-    ]
-  }
-]
   });
   // Grab the user's input from the city-search input field. Store it in the input variable.
   input = document.getElementById('city-search');
@@ -306,7 +302,7 @@ function initAutocomplete() {
     markers.forEach(function(marker) {
       marker.setMap(null);
     });
-    // For each place (only should apply to 1 in our case - change this), get the icon, name and location.
+    // For each place (only should apply to 1 in our case), get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
       if (!place.geometry) {
@@ -336,34 +332,35 @@ function initAutocomplete() {
     });
     map.fitBounds(bounds);
   });
-
-
 }
 
 
 /////////////////////////
 // Main Controller
 /////////////////////////
-
-
 app.controller('mainController', ['$http', '$scope', '$filter', function($http, $scope, $filter) {
-
+  // Declare variables
   let self = this;
-  let lat;
-  let long;
-
   this.url      = 'http://localhost:3000';
-  this.testing  = "Hello Bluewolf";
   this.user     = {};
   this.noMatch  = false;
   this.forecast = '';
   this.home = true;
-
+  this.lat = 0;
+  this.lng = 0;
+  this.location = '';
+  this.myLocations = [];
+  // GET WEATHER
+  // Store the input values of searched location in variables so that they can be referenced outside this function
+  // GET request to Dark Sky API returns forecast
   this.getWeather = () => {
-    let lat = document.getElementById('latitude').value;
-    let long = document.getElementById('longitude').value;
+    lat = document.getElementById('latitude').value;
+    long = document.getElementById('longitude').value;
     // make the api call
-    // console.log('https://api.darksky.net/forecast/a9d279d680c92a498e132e03592ee873' + '/' + lat + ',' + long + '?units=auto');
+    this.lat = document.getElementById('latitude').value;
+    this.lng = document.getElementById('longitude').value;
+    this.location = document.getElementById('city-search').value;
+    console.log('https://api.darksky.net/forecast/a9d279d680c92a498e132e03592ee873' + '/' + lat + ',' + long + '?units=auto');
     $http({
       method: 'GET',
       // url: this.url + '/forecasts/index',
@@ -372,12 +369,18 @@ app.controller('mainController', ['$http', '$scope', '$filter', function($http, 
     }).then( response => {
       console.log(response);
       this.forecast = response.data;
+      // Get the current skycon value
       let currentIcon = response.data.currently.icon;
-      console.log(currentIcon);
+      // console.log(currentIcon);
+      // console.log(this.lat, "lat");
+      // console.log(this.lng, "lng");
+      // console.log(this.location, "location");
+      // Set the skycon
       self.setSkycon(currentIcon);
     });
   }
 
+  // Play whichever skycon is referenced in the forecast.
   this.setSkycon = (currentIcon) => {
     let skycon = new Skycons({"color": "whitesmoke"});
     if (currentIcon==="partly-cloudy-night"){
@@ -404,7 +407,48 @@ app.controller('mainController', ['$http', '$scope', '$filter', function($http, 
     skycon.play();
   }
 
-  // User Login Route
+  // SAVE LOCATION
+  // Sends POST request to server
+  // Saves the most recently forecasted location with latitude, longitude, location name, and user's ID
+  this.saveLocation = (lat, long, location) => {
+    // console.log('called saveLocation()');
+    $http({
+      method: 'POST',
+      url: this.url + '/locations/',
+      data: {
+        location: {
+          lat: this.lat,
+          lng: this.lng,
+          location_name: this.location,
+          user_id: this.user.id
+        }
+      }
+    }).then( response => {
+      // console.log('processed');
+      // console.log(this.lat, "lat");
+      // console.log(this.lng, "lng");
+      // console.log(this.location, "location");
+      console.log(response);
+      this.myLocations.push(response.data);
+    } )
+  }
+
+  // Get the user's saved locations upon login
+  this.getLocations = () => {
+    $http({
+      method: 'GET',
+      url: this.url + '/locations/',
+    }).then( response => {
+      console.log(response);
+      for (var i=0; i<response.data.length; i++) {
+        if (response.data[i].user_id === this.user.id) {
+          this.myLocations.unshift(response.data[i]);
+        }
+      }
+    })
+  }
+
+  // LOGIN
   this.login = (userPass) => {
     $http({
       method: 'POST',
@@ -419,9 +463,11 @@ app.controller('mainController', ['$http', '$scope', '$filter', function($http, 
       localStorage.setItem('token', JSON.stringify(response.data.token));
       console.log(localStorage.token);
       this.home = false;
+      this.getLocations();
     });
   }
 
+  // REGISTER
   this.register = (userReg) => {
     if (userReg.password === userReg.confirmPassword) {
       console.log("passwords match");
@@ -443,6 +489,7 @@ app.controller('mainController', ['$http', '$scope', '$filter', function($http, 
     })
   }
 
+  // AUTHORIZATION
   this.getUsers = () => {
     $http({
       url: this.url + '/users/',
@@ -460,6 +507,7 @@ app.controller('mainController', ['$http', '$scope', '$filter', function($http, 
     })
   }
 
+  // LOGOUT
   this.logout = () => {
     localStorage.clear('token');
     location.reload();
